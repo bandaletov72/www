51cf2b112947623c58b6470e5e7754e1e3de2cc8
Pavel A. Bandaletov <bandaletov72@gmail.com>
2013-10-09 21:16:50 +0400

    Widgets settings
    You may add your custom widget here

---
+++ widgets/list.php
@@ -0,0 +1,14 @@
+<?php
+$cityname="Moscow";
+$citycode=27612; // Citycode can found here: http://www.24ok.ru/index.php?page=nastroyka_vidjetov_pogodyi_i_zakata_voshoda
+$lat = 55.76;    // City Lattitude
+$long = 37.62;   // City Longtitude
+$offset = 4;     // difference between GMT and local time in hours
+$refresh_interval = 3600; // in seconds, 1 hour = 3600 sec
+require ("./widgets/weather.inc");
+if (function_exists ('date_sunrise')) {
+require ("./widgets/sunset.inc");
+}
+//require ("./widgets/exit.inc");
+
+?>

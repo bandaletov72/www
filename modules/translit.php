4c5c0946ea41542d30ca7901d5026fe58ce01d29
Pavel A. Bandaletov <bandaletov72@gmail.com>
2013-10-09 21:32:00 +0400

    Translit chars functions

---
+++ modules/translit.php
@@ -0,0 +1,483 @@
+<?php
+$metat="";
+
+function toHache($str) {
+global $hache_salt;
+$ret="";
+$r=$hache_salt.$hache_salt.$hache_salt.$hache_salt.$hache_salt.$hache_salt;
+$md=md5($str);
+for ($i=0; $i<16; $i++) {
+$mmd=substr($md,0,2);
+$md=substr($md,2);
+$ret.=substr($r,hexdec($mmd),1);
+}
+return $ret;
+}
+function toRus($str) {
+$str = toLower($str);
+$trans = array(
+"q" => "é",
+"w" => "ö",
+"e" => "ó",
+"r" => "ê",
+"t" => "å",
+"y" => "í",
+"u" => "ã",
+"i" => "ø",
+"o" => "ù",
+"p" => "ç",
+"[" => "õ",
+"]" => "ú",
+"a" => "ô",
+"s" => "û",
+"d" => "â",
+"f" => "à",
+"g" => "ï",
+"h" => "ð",
+"j" => "î",
+"k" => "ë",
+"l" => "ä",
+";" => "æ",
+"'" => "ý",
+"z" => "ÿ",
+"x" => "÷",
+"c" => "ñ",
+"v" => "ì",
+"b" => "è",
+"n" => "ò",
+"m" => "ü",
+"," => "á",
+"." => "þ");
+$str=strtr( $str, $trans);
+   return strtolower($str);
+}
+function toEng($str) {
+$str = toLower($str);
+$trans = array(
+"é" => "q",
+"ö" => "w",
+"ó" => "e",
+"ê" => "r",
+"å" => "t",
+"í" => "y",
+"ã" => "u",
+"ø" => "i",
+"ù" => "o",
+"ç" => "p",
+"õ" => "[",
+"ú" => "]",
+"ô" => "a",
+"û" => "s",
+"â" => "d",
+"à" => "f",
+"ï" => "g",
+"ð" => "h",
+"î" => "j",
+"ë" => "k",
+"ä" => "l",
+"æ" => ";",
+"ý" => "'",
+"ÿ" => "z",
+"÷" => "x",
+"ñ" => "c",
+"ì" => "v",
+"è" => "b",
+"ò" => "n",
+"ü" => "m",
+"á" => ",",
+"þ" => ".",
+"¸" => "");
+$str=strtr( $str, $trans);
+return strtolower($str);
+}
+function toFirst($str) {
+$first=substr($str,0,1);
+$next=substr($str,1,(strlen($str)-1));
+$next = strtr($next, "ÀÁÂÃÄÅ¨ÆÇÈÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÜÚÛÝÞß",
+"àáâãäååæçèêëìíîïðñòóôõö÷øùüúûýþÿ");
+$next=strtolower($next);
+$first = strtr($first, "àáâãäååæçèêëìíîïðñòóôõö÷øùüúûýþÿ",
+"ÀÁÂÃÄÅ¨ÆÇÈÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÜÚÛÝÞß");
+$first=strtoupper($first);
+return ($first.$next);
+}
+function translit($strw, $z=0) {
+global $codepage;
+$strw = strip_tags($strw);
+if (($codepage=="windows-1250")||($codepage=="windows-1252")||($codepage=="windows-1253")||($codepage=="windows-1254")||($codepage=="windows-1255")||($codepage=="windows-1257")) {$strw=iconv($codepage, 'ASCII//TRANSLIT', "$strw");}
+$strw = str_replace("&","",$strw);
+$strw = str_replace("'","",$strw);
+$strw = str_replace("%","_proc",$strw);
+$strw = str_replace("`","",$strw);
+$strw = str_replace(";","_",$strw);
+$strw = str_replace("#","",$strw);
+$strw = str_replace("~","",$strw);
+
+$strw = str_replace(chr(0x27),"",$strw);
+$strw = str_replace(chr(0x60),"",$strw);
+$strw = str_replace(chr(0x80),"d",$strw);
+$strw = str_replace(chr(0x90),"d",$strw);
+$strw = str_replace(chr(0x81),"g",$strw);
+$strw = str_replace(chr(0x82),"",$strw);
+$strw = str_replace(chr(0x83),"g",$strw);
+$strw = str_replace(chr(0x84),"",$strw);
+$strw = str_replace(chr(0x85),"",$strw);
+$strw = str_replace(chr(0x86),"",$strw);
+$strw = str_replace(chr(0x87),"",$strw);
+$strw = str_replace(chr(0x88),"euro",$strw);
+$strw = str_replace(chr(0x89),"pr",$strw);
+$strw = str_replace(chr(0x8A),"l",$strw);
+$strw = str_replace(chr(0x8B),"",$strw);
+$strw = str_replace(chr(0x8C),"n",$strw);
+$strw = str_replace(chr(0x8D),"k",$strw);
+$strw = str_replace(chr(0x8E),"t",$strw);
+$strw = str_replace(chr(0x8F),"d",$strw);
+$strw = str_replace(chr(0x90),"d",$strw);
+$strw = str_replace(chr(0x91),"",$strw);
+$strw = str_replace(chr(0x92),"",$strw);
+$strw = str_replace(chr(0x93),"",$strw);
+$strw = str_replace(chr(0x94),"",$strw);
+$strw = str_replace(chr(0x95),"",$strw);
+$strw = str_replace(chr(0x96),"",$strw);
+$strw = str_replace(chr(0x97),"",$strw);
+$strw = str_replace(chr(0x98),"",$strw);
+$strw = str_replace(chr(0x99),"",$strw);
+$strw = str_replace(chr(0x9A),"l",$strw);
+$strw = str_replace(chr(0x9B),"",$strw);
+$strw = str_replace(chr(0x9C),"n",$strw);
+$strw = str_replace(chr(0x9D),"k",$strw);
+$strw = str_replace(chr(0x9E),"t",$strw);
+$strw = str_replace(chr(0x9F),"d",$strw);
+$strw = str_replace(chr(0xA1),"u",$strw);
+$strw = str_replace(chr(0xA2),"u",$strw);
+$strw = str_replace(chr(0xA3),"j",$strw);
+$strw = str_replace(chr(0xA4),"j",$strw);
+$strw = str_replace(chr(0xA5),"g",$strw);
+$strw = str_replace(chr(0xA6),"",$strw);
+$strw = str_replace(chr(0xA7),"",$strw);
+$strw = str_replace(chr(0xA8),"yo",$strw);
+$strw = str_replace(chr(0xA9),"c",$strw);
+$strw = str_replace(chr(0xAA),"e",$strw);
+$strw = str_replace(chr(0xAB),"",$strw);
+$strw = str_replace(chr(0xAC),"",$strw);
+$strw = str_replace(chr(0xAD),"_",$strw);
+$strw = str_replace(chr(0xAE),"r",$strw);
+$strw = str_replace(chr(0xAF),"yi",$strw);
+$strw = str_replace(chr(0xB0),"",$strw);
+$strw = str_replace(chr(0xB1),"",$strw);
+$strw = str_replace(chr(0xB2),"i",$strw);
+$strw = str_replace(chr(0xB3),"i",$strw);
+$strw = str_replace(chr(0xB4),"g",$strw);
+$strw = str_replace(chr(0xB5),"m",$strw);
+$strw = str_replace(chr(0xB6),"",$strw);
+$strw = str_replace(chr(0xB7),"",$strw);
+$strw = str_replace(chr(0xB8),"yo",$strw);
+$strw = str_replace(chr(0xB9),"num",$strw);
+$strw = str_replace(chr(0xBA),"e",$strw);
+$strw = str_replace(chr(0xBB),"",$strw);
+$strw = str_replace(chr(0xBC),"j",$strw);
+$strw = str_replace(chr(0xBD),"j",$strw);
+$strw = str_replace(chr(0xBE),"j",$strw);
+$strw = str_replace(chr(0xBF),"yi",$strw);
+
+
+
+
+$strw = htmlspecialchars($strw);
+
+$strw = str_replace(";","_",$strw);
+$strw = str_replace("`","",$strw);
+$strw = str_replace("&","",$strw);
+$strw = str_replace("'","",$strw);
+$strw = str_replace("%","",$strw);
+$strw = str_replace("#","",$strw);
+$strw = str_replace(" ","_",$strw);
+$strw = str_replace(",","_",$strw);
+if ($z==0) {
+$strw = str_replace(".","",$strw);
+}
+$strw = str_replace("!","",$strw);
+$strw = str_replace("-","_",$strw);
+$strw = str_replace("`","",$strw);
+$strw = str_replace("¹","",$strw);
+$strw = str_replace("~","",$strw);
+$strw = str_replace("@","",$strw);
+$strw = str_replace("\$","",$strw);
+$strw = str_replace("}","",$strw);
+$strw = str_replace("{","_",$strw);
+$strw = str_replace("]","",$strw);
+$strw = str_replace("[","_",$strw);
+$strw = str_replace("^","",$strw);
+$strw = str_replace("*","x",$strw);
+$strw = str_replace(";","_",$strw);
+$strw = str_replace("+","",$strw);
+$strw = str_replace("=","",$strw);
+$strw = str_replace(":","",$strw);
+$strw = str_replace("(","_",$strw);
+$strw = str_replace(")","",$strw);
+$strw = str_replace("/","_",$strw);
+$strw = str_replace("?","",$strw);
+$strw = str_replace("\\","",$strw);
+$strw = str_replace("|","_",$strw);
+$strw = str_replace("\"","",$strw);
+$strw = str_replace("\'","",$strw);
+$strw = str_replace("à","a",$strw);
+$strw = str_replace("á","b",$strw);
+$strw = str_replace("â","v",$strw);
+$strw = str_replace("ã","g",$strw);
+$strw = str_replace("ä","d",$strw);
+$strw = str_replace("å","e",$strw);
+$strw = str_replace("¸","yo",$strw);
+$strw = str_replace("æ","j",$strw);
+$strw = str_replace("ç","z",$strw);
+$strw = str_replace("è","i",$strw);
+$strw = str_replace("é","y",$strw);
+$strw = str_replace("ê","k",$strw);
+$strw = str_replace("ë","l",$strw);
+$strw = str_replace("ì","m",$strw);
+$strw = str_replace("í","n",$strw);
+$strw = str_replace("î","o",$strw);
+$strw = str_replace("ï","p",$strw);
+$strw = str_replace("ð","r",$strw);
+$strw = str_replace("ñ","s",$strw);
+$strw = str_replace("ò","t",$strw);
+$strw = str_replace("ó","u",$strw);
+$strw = str_replace("ô","f",$strw);
+$strw = str_replace("õ","h",$strw);
+$strw = str_replace("ö","tz",$strw);
+$strw = str_replace("÷","ch",$strw);
+$strw = str_replace("ø","sh",$strw);
+$strw = str_replace("ù","sch",$strw);
+$strw = str_replace("ú","",$strw);
+$strw = str_replace("û","yi",$strw);
+$strw = str_replace("ü","",$strw);
+$strw = str_replace("ý","e",$strw);
+$strw = str_replace("þ","u",$strw);
+$strw = str_replace("ÿ","ya",$strw);
+$strw = str_replace("À","a",$strw);
+$strw = str_replace("Á","b",$strw);
+$strw = str_replace("Â","v",$strw);
+$strw = str_replace("Ã","g",$strw);
+$strw = str_replace("Ä","d",$strw);
+$strw = str_replace("Å","e",$strw);
+$strw = str_replace("¨","yo",$strw);
+$strw = str_replace("Æ","j",$strw);
+$strw = str_replace("Ç","z",$strw);
+$strw = str_replace("È","i",$strw);
+$strw = str_replace("É","y",$strw);
+$strw = str_replace("Ê","k",$strw);
+$strw = str_replace("Ë","l",$strw);
+$strw = str_replace("Ì","m",$strw);
+$strw = str_replace("Í","n",$strw);
+$strw = str_replace("Î","o",$strw);
+$strw = str_replace("Ï","p",$strw);
+$strw = str_replace("Ð","r",$strw);
+$strw = str_replace("Ñ","s",$strw);
+$strw = str_replace("Ò","t",$strw);
+$strw = str_replace("Ó","u",$strw);
+$strw = str_replace("Ô","f",$strw);
+$strw = str_replace("Õ","h",$strw);
+$strw = str_replace("Ö","tz",$strw);
+$strw = str_replace("×","ch",$strw);
+$strw = str_replace("Ø","sh",$strw);
+$strw = str_replace("Ù","sch",$strw);
+$strw = str_replace("Ú","",$strw);
+$strw = str_replace("Û","yi",$strw);
+$strw = str_replace("Ü","",$strw);
+$strw = str_replace("Ý","e",$strw);
+$strw = str_replace("Þ","u",$strw);
+$strw = str_replace("ß","ya",$strw);
+$strw = strtolower($strw);
+
+return $strw;
+
+}
+
+
+function translit0($strw) {
+global $codepage;
+$strw = strip_tags($strw);
+if (($codepage=="windows-1250")||($codepage=="windows-1252")||($codepage=="windows-1253")||($codepage=="windows-1254")||($codepage=="windows-1255")||($codepage=="windows-1257")) {$strw=iconv($codepage, 'ASCII//TRANSLIT', "$strw");}
+$strw = str_replace("&","",$strw);
+$strw = str_replace("'","",$strw);
+$strw = str_replace("%","_proc",$strw);
+$strw = str_replace("`","",$strw);
+$strw = str_replace(";","_",$strw);
+$strw = str_replace("#","",$strw);
+$strw = str_replace("~","",$strw);
+
+$strw = str_replace(chr(0x27),"",$strw);
+$strw = str_replace(chr(0x60),"",$strw);
+$strw = str_replace(chr(0x80),"d",$strw);
+$strw = str_replace(chr(0x90),"d",$strw);
+$strw = str_replace(chr(0x81),"g",$strw);
+$strw = str_replace(chr(0x82),"",$strw);
+$strw = str_replace(chr(0x83),"g",$strw);
+$strw = str_replace(chr(0x84),"",$strw);
+$strw = str_replace(chr(0x85),"",$strw);
+$strw = str_replace(chr(0x86),"",$strw);
+$strw = str_replace(chr(0x87),"",$strw);
+$strw = str_replace(chr(0x88),"euro",$strw);
+$strw = str_replace(chr(0x89),"pr",$strw);
+$strw = str_replace(chr(0x8A),"l",$strw);
+$strw = str_replace(chr(0x8B),"",$strw);
+$strw = str_replace(chr(0x8C),"n",$strw);
+$strw = str_replace(chr(0x8D),"k",$strw);
+$strw = str_replace(chr(0x8E),"t",$strw);
+$strw = str_replace(chr(0x8F),"d",$strw);
+$strw = str_replace(chr(0x90),"d",$strw);
+$strw = str_replace(chr(0x91),"",$strw);
+$strw = str_replace(chr(0x92),"",$strw);
+$strw = str_replace(chr(0x93),"",$strw);
+$strw = str_replace(chr(0x94),"",$strw);
+$strw = str_replace(chr(0x95),"",$strw);
+$strw = str_replace(chr(0x96),"",$strw);
+$strw = str_replace(chr(0x97),"",$strw);
+$strw = str_replace(chr(0x98),"",$strw);
+$strw = str_replace(chr(0x99),"",$strw);
+$strw = str_replace(chr(0x9A),"l",$strw);
+$strw = str_replace(chr(0x9B),"",$strw);
+$strw = str_replace(chr(0x9C),"n",$strw);
+$strw = str_replace(chr(0x9D),"k",$strw);
+$strw = str_replace(chr(0x9E),"t",$strw);
+$strw = str_replace(chr(0x9F),"d",$strw);
+$strw = str_replace(chr(0xA1),"u",$strw);
+$strw = str_replace(chr(0xA2),"u",$strw);
+$strw = str_replace(chr(0xA3),"j",$strw);
+$strw = str_replace(chr(0xA4),"j",$strw);
+$strw = str_replace(chr(0xA5),"g",$strw);
+$strw = str_replace(chr(0xA6),"",$strw);
+$strw = str_replace(chr(0xA7),"",$strw);
+$strw = str_replace(chr(0xA8),"yo",$strw);
+$strw = str_replace(chr(0xA9),"c",$strw);
+$strw = str_replace(chr(0xAA),"e",$strw);
+$strw = str_replace(chr(0xAB),"",$strw);
+$strw = str_replace(chr(0xAC),"",$strw);
+$strw = str_replace(chr(0xAD),"_",$strw);
+$strw = str_replace(chr(0xAE),"r",$strw);
+$strw = str_replace(chr(0xAF),"yi",$strw);
+$strw = str_replace(chr(0xB0),"",$strw);
+$strw = str_replace(chr(0xB1),"",$strw);
+$strw = str_replace(chr(0xB2),"i",$strw);
+$strw = str_replace(chr(0xB3),"i",$strw);
+$strw = str_replace(chr(0xB4),"g",$strw);
+$strw = str_replace(chr(0xB5),"m",$strw);
+$strw = str_replace(chr(0xB6),"",$strw);
+$strw = str_replace(chr(0xB7),"",$strw);
+$strw = str_replace(chr(0xB8),"yo",$strw);
+$strw = str_replace(chr(0xB9),"num",$strw);
+$strw = str_replace(chr(0xBA),"e",$strw);
+$strw = str_replace(chr(0xBB),"",$strw);
+$strw = str_replace(chr(0xBC),"j",$strw);
+$strw = str_replace(chr(0xBD),"j",$strw);
+$strw = str_replace(chr(0xBE),"j",$strw);
+$strw = str_replace(chr(0xBF),"yi",$strw);
+
+$strw = htmlspecialchars($strw);
+
+$strw = str_replace(";","_",$strw);
+$strw = str_replace("`","",$strw);
+$strw = str_replace("&","",$strw);
+$strw = str_replace("'","",$strw);
+$strw = str_replace("%","",$strw);
+$strw = str_replace("#","",$strw);
+$strw = str_replace(" ","_",$strw);
+$strw = str_replace(",","_",$strw);
+$strw = str_replace(".","",$strw);
+$strw = str_replace("!","",$strw);
+$strw = str_replace("-","_",$strw);
+$strw = str_replace("`","",$strw);
+$strw = str_replace("¹","",$strw);
+$strw = str_replace("~","",$strw);
+$strw = str_replace("@","",$strw);
+$strw = str_replace("\$","",$strw);
+$strw = str_replace("}","",$strw);
+$strw = str_replace("{","_",$strw);
+$strw = str_replace("]","",$strw);
+$strw = str_replace("[","_",$strw);
+$strw = str_replace("^","",$strw);
+$strw = str_replace("*","x",$strw);
+$strw = str_replace(";","_",$strw);
+$strw = str_replace("+","",$strw);
+$strw = str_replace("=","",$strw);
+$strw = str_replace(":","",$strw);
+$strw = str_replace("(","_",$strw);
+$strw = str_replace(")","",$strw);
+$strw = str_replace("/","_",$strw);
+$strw = str_replace("?","",$strw);
+$strw = str_replace("\\","",$strw);
+$strw = str_replace("|","_",$strw);
+$strw = str_replace("\"","",$strw);
+$strw = str_replace("\'","",$strw);
+$strw = str_replace("à","a",$strw);
+$strw = str_replace("á","b",$strw);
+$strw = str_replace("â","v",$strw);
+$strw = str_replace("ã","g",$strw);
+$strw = str_replace("ä","d",$strw);
+$strw = str_replace("å","e",$strw);
+$strw = str_replace("¸","yo",$strw);
+$strw = str_replace("æ","j",$strw);
+$strw = str_replace("ç","z",$strw);
+$strw = str_replace("è","i",$strw);
+$strw = str_replace("é","y",$strw);
+$strw = str_replace("ê","k",$strw);
+$strw = str_replace("ë","l",$strw);
+$strw = str_replace("ì","m",$strw);
+$strw = str_replace("í","n",$strw);
+$strw = str_replace("î","o",$strw);
+$strw = str_replace("ï","p",$strw);
+$strw = str_replace("ð","r",$strw);
+$strw = str_replace("ñ","s",$strw);
+$strw = str_replace("ò","t",$strw);
+$strw = str_replace("ó","u",$strw);
+$strw = str_replace("ô","f",$strw);
+$strw = str_replace("õ","h",$strw);
+$strw = str_replace("ö","tz",$strw);
+$strw = str_replace("÷","ch",$strw);
+$strw = str_replace("ø","sh",$strw);
+$strw = str_replace("ù","sch",$strw);
+$strw = str_replace("ú","",$strw);
+$strw = str_replace("û","yi",$strw);
+$strw = str_replace("ü","",$strw);
+$strw = str_replace("ý","e",$strw);
+$strw = str_replace("þ","u",$strw);
+$strw = str_replace("ÿ","ya",$strw);
+$strw = str_replace("À","a",$strw);
+$strw = str_replace("Á","b",$strw);
+$strw = str_replace("Â","v",$strw);
+$strw = str_replace("Ã","g",$strw);
+$strw = str_replace("Ä","d",$strw);
+$strw = str_replace("Å","e",$strw);
+$strw = str_replace("¨","yo",$strw);
+$strw = str_replace("Æ","j",$strw);
+$strw = str_replace("Ç","z",$strw);
+$strw = str_replace("È","i",$strw);
+$strw = str_replace("É","y",$strw);
+$strw = str_replace("Ê","k",$strw);
+$strw = str_replace("Ë","l",$strw);
+$strw = str_replace("Ì","m",$strw);
+$strw = str_replace("Í","n",$strw);
+$strw = str_replace("Î","o",$strw);
+$strw = str_replace("Ï","p",$strw);
+$strw = str_replace("Ð","r",$strw);
+$strw = str_replace("Ñ","s",$strw);
+$strw = str_replace("Ò","t",$strw);
+$strw = str_replace("Ó","u",$strw);
+$strw = str_replace("Ô","f",$strw);
+$strw = str_replace("Õ","h",$strw);
+$strw = str_replace("Ö","tz",$strw);
+$strw = str_replace("×","ch",$strw);
+$strw = str_replace("Ø","sh",$strw);
+$strw = str_replace("Ù","sch",$strw);
+$strw = str_replace("Ú","",$strw);
+$strw = str_replace("Û","yi",$strw);
+$strw = str_replace("Ü","",$strw);
+$strw = str_replace("Ý","e",$strw);
+$strw = str_replace("Þ","u",$strw);
+$strw = str_replace("ß","ya",$strw);
+//$strw = strtolower($strw);
+
+return $strw;
+
+}
+?>
